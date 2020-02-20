3fa1c305374e7cf8cf9380121542e80ffe0ecc0d
Angel Nuñez <snahider@gmail.com>
2020-02-20 01:32:46 -0500

    config without writing in workspace

--- vars/pipelineConfig.groovy
+++ vars/pipelineConfig.groovy
@@ -1,8 +1,8 @@
 def call(){
     def configFileName = 'pipeline-config.yaml'
     def config = libraryResource configFileName
-    writeFile file: "${WORKSPACE}/${configFileName}", text: text
+    //writeFile file: "${WORKSPACE}/${configFileName}", text: text
 
-    Map pipelineCfg = readYaml(file: "${WORKSPACE}/${configFileName}")
+    Map pipelineCfg = readYaml(text: config)
     return pipelineCfg
 }
